2ebfb999dd1b0ae577bbb154b7b03b9570c8eb78
Pedro Xavier <pedro.xavier@peakit.pt>
2021-01-26 20:24:59 +0000

    Initial commit class 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,50 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const QuizContainer = styled.div`
+	width: 100%;
+	max-width: 350px;
+	padding-top: 45px;
+	margin: auto 10%;
+	@media screen and (max-width: 500px) {
+		margin: auto;
+		padding: 15px;
+	}
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+	return (
+		<QuizBackground backgroundImage={db.bg}>
+			<QuizContainer>
+				<Widget>
+					<Widget.Header>
+						<h1>What is Lorem Ipsum?</h1>
+					</Widget.Header>
+					<Widget.Content>
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry.
+						</p>
+					</Widget.Content>
+				</Widget>
+				<Widget>
+					<Widget.Header>
+						<h1>Why do we use it?</h1>
+					</Widget.Header>
+					<Widget.Content>
+						<p>
+							It is a long established fact that a reader will be distracted by
+							the readable content of a page when looking at its layout.
+						</p>
+					</Widget.Content>
+				</Widget>
+				<Footer />
+			</QuizContainer>
+			<GitHubCorner projectUrl="https://github.com/PedroXavier14" />
+		</QuizBackground>
+	);
 }
